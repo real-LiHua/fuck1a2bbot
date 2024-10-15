9abd774967119ef3a621c20a9060e2690d092a1c
Li Hua <lihua@email.com>
2024-10-15 14:10:19 +0800

    perf: null

--- fuck1a2bbot/__init__.py
+++ fuck1a2bbot/__init__.py
@@ -1,5 +1,6 @@
 import re
 from itertools import permutations
+from string import digits
 
 from telegram import Message, Update
 from telegram.ext import ContextTypes
@@ -19,20 +20,18 @@ async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg: Message = update.effective_message.reply_to_message
     if not msg or not msg.text.startswith("猜测历史："):
         return
-    
+
     filters: list = re.findall(r"(\d+) (\dA\d+B)", msg.text)
     if not filters:
         return
-    
+
     text: str = update.effective_message.text
     flag: int = int(text.split()[1]) if len(text.split()) >= 2 else 0
-    
+
     result: list = []
-    for candidate in map(
-        "".join, permutations(map(str, range(10)), len(filters[0][0]))
-    ):
+    for candidate in map("".join, permutations(digits, len(filters[0][0]))):
         for item in filters:
-            if candidate != item[0] and compare(candidate, item[0]) != item[1]:
+            if candidate == item[0] or compare(candidate, item[0]) != item[1]:
                 break
         else:
             result.append(candidate)
